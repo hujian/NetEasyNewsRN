8f32306a2e4f3bd84f8512ac40b4cfb714458755
hj <hujian2009@gmail.com>
2016-07-17 20:32:08 +0800

    first commit

---
+++ src/index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+var MOCKED_NEWS_DATA = [
+  {title: '今天天气好', date: '2015', thumbnail: 'http://i.imgur.com/UePbdph.jpg'},
+];
+
+class NetEasyNewsRN extends Component {
+  render() {
+    var news = MOCKED_NEWS_DATA[0];
+
+    return (
+      <View style={styles.container}>
+        <Text>{news.title}</Text>
+        <Text>{news.date}</Text>
+        <Image 
+          source={{uri:news.thumbnail}}
+          style={styles.thumbnail}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  }
+});
+
+AppRegistry.registerComponent('NetEasyNewsRN', () => NetEasyNewsRN);
