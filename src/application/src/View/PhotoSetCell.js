28b599713562fa0c81e6bedb1d15b5b205ed19ef
hj <hujian2009@gmail.com>
2016-07-27 10:17:09 +0800

    加入swiper控件和icon

---
+++ src/application/src/View/PhotoSetCell.js
@@ -0,0 +1,23 @@
+/**
+ * @brief:  大图滚动cell
+ * @author: hj
+ * @date:   2016-07-20
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import {
+	Text,
+	Image,
+} from 'react-native';
+
+export default class PhotoCell extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+		);
+	}
+};
